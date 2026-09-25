71cdb9ab897962d5762524af9ead86a4fc02720d
kr-Satyam09 <satyam.kr151237@gmail.com>
2026-09-25 23:58:32 +0530

    Add getSectorKey function for unique sector key generation

--- Phase2/AVL_Tree.h
+++ Phase2/AVL_Tree.h
@@ -10,3 +10,11 @@ struct AVLNode {
     AVLNode *right;
     int height;
 };
+
+// It generates a unique sector key for spatial mapping
+inline long long getSectorKey(Vector3 p) {
+    long long bx = (long long) std::floor(p.x / 100.0);
+    long long by = (long long) std::floor(p.y / 100.0);
+    long long bz = (long long) std::floor(p.z / 100.0);
+    return bx * 1000000LL + by * 1000LL + bz;
+}
